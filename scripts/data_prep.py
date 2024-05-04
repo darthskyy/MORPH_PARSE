ed342b61381c2ea1caa240fa092c5b50ed37661b
Restioson <restiosondev@gmail.com>
2024-05-05 00:40:22 +0200

    feat: split words into canonical segmentations and tags in .tsv

--- scripts/data_prep.py
+++ scripts/data_prep.py
@@ -2,33 +2,37 @@ import re
 import os
 
 dirs = ['TEST', 'TRAIN']
-in_name_format = 'SADII.{0}.Morph_Lemma_POS.1.0.0.{1}.CTexT.TG.2021-09-30'
 out_name_format = "{0}_{1}.tsv"
+
 
 def read_lines(file_path: str) -> list:
     """Reads the lines of a file and returns them as a list."""
     with open(file_path, 'r') as f:
         return f.readlines()
 
+
 def format_line(line: str) -> str:
     """
     Removes the Lemma and POS columns from the line then adds the canonical segmentation of the word without the morphological tags.
     """
     line = line.rstrip()
-    line = line.split('\t')[:2]
-    line.append(line[1])
-    line[1] = strip_tags(line[1])
-
+    raw, parsed = line.split()[:2]
+    segmentation, tags = split_tags(parsed)
+    line = [raw, parsed, '_'.join(segmentation), '_'.join(tags)]
     return '\t'.join(line)
 
-def strip_tags(text: str) -> str:
-    """Removes morphological tags from a parsed word."""
-    return re.sub(r'\[.*?\]', '', text)
+
+def split_tags(text: str) -> (str, list[str]):
+    """Split a word into its canonical segmentation and morpheme tags."""
+    split = [morpheme for morpheme in re.split(r'\[[a-zA-Z-_0-9]*?]-?', text) if morpheme != ""]
+    return (split, re.findall(r'\[([a-zA-Z-_0-9]*?)]', text))
+
 
 def write_lines(file_path: str, lines: list) -> None:
     """Writes a list of lines to a file."""
     with open(file_path, 'w') as f:
         f.write('\n'.join(lines))
+
 
 def main():
     # assumes that the directory structure is as follows:
@@ -41,11 +45,11 @@ def main():
     #   data_prep.py
 
     # outputs the files in the following format:
-    # word{tab}canonical_segmentation{tab}morpholgical_parse
+    # word{tab}canonical_segmentation{tab}morphological_parse
     for dir in dirs:
         for in_file in os.listdir(f'../data/{dir}'):
             # Skip the English files
-            if 'EN' in in_file:
+            if 'EN' in in_file or not in_file.endswith('.txt'):
                 continue
 
             # Get the language of the file
