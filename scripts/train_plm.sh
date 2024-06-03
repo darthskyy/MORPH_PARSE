83972e9cfc7cc9aabf5522074d8b21649cacfeec
Simbarashe Mawere <simba.mawere@gmail.com>
2024-06-03 16:47:52 +0200

    updated the training script to use GPUs and loop over all four of the languages

--- scripts/train_plm.sh
+++ scripts/train_plm.sh
@@ -17,36 +17,38 @@ module load chpc/cuda/11.2/PCIe/11.2
 echo "Installing requirements"
 pip3 install -r requirements.txt
 
-echo "Training PLM"
 data=data
 checkpoint=xlm-roberta-large
-lang=XH
-output_dir=plm/models/${checkpoint}_${lang}
 epochs=10
 batch_size=16
 evaluation_strategy=steps
 learning_rate=2e-5
 validation_split=0.1
 save_steps=2500
-save_total_limit=2
+save_total_limit=1
 load_best_model_at_end=True
 metric_for_best_model=loss
 greater_is_better=False
 resume_from_checkpoint=True
 
-python3 plm/train.py \
-    --data $data \
-    --checkpoint $checkpoint \
-    --output $output_dir \
-    --epochs $epochs \
-    --batch_size $batch_size \
-    --evaluation_strategy $evaluation_strategy \
-    --lang $lang \
-    --learning_rate $learning_rate \
-    --validation_split $validation_split \
-    --save_steps $save_steps \
-    --save_total_limit $save_total_limit \
-    --load_best_model_at_end \
-    --metric_for_best_model $metric_for_best_model \
-    --greater_is_better $greater_is_better \
-    --resume_from_checkpoint
+for lang in NR SS XH ZU; do
+    echo "Training PLM for $lang"
+    output_dir=plm/models/run2/${checkpoint}_${lang}
+    python3 plm/train.py \
+        --data $data \
+        --checkpoint $checkpoint \
+        --output $output_dir \
+        --epochs $epochs \
+        --batch_size $batch_size \
+        --evaluation_strategy $evaluation_strategy \
+        --lang $lang \
+        --learning_rate $learning_rate \
+        --validation_split $validation_split \
+        --save_steps $save_steps \
+        --save_total_limit $save_total_limit \
+        --load_best_model_at_end \
+        --metric_for_best_model $metric_for_best_model \
+        --greater_is_better $greater_is_better \
+        --resume_from_checkpoint \
+        --log train${lang}.log
+done
