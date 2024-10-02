bbb234ddae08477f143476f8ec65c347f1eb5080
Restioson <restiosondev@gmail.com>
2024-10-02 20:00:44 +0200

    Responsiveness on small width devices

--- docs/script.js
+++ docs/script.js
@@ -1,25 +1,41 @@
-// getting all the sections and nav links
-const sections = document.querySelectorAll('section');
-const navLinks = document.querySelectorAll('nav ul li a');
+window.addEventListener("DOMContentLoaded", function () {
+    // getting all the sections and nav links
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav ul li a');
 
-// Detecting the current section and adding active class to the nav link
-function activateSection() {
-    let currentSection = '';
+    // Detecting the current section and adding active class to the nav link
+    function activateSection() {
+        let currentSection = '';
 
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        if (pageYOffset >= sectionTop - 60) {
-            currentSection = section.getAttribute('id');
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            if (pageYOffset >= sectionTop - 60) {
+                currentSection = section.getAttribute('id');
+            }
+        });
+
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if (link.getAttribute('href').includes(currentSection)) {
+                link.classList.add('active');
+            }
+        });
+    }
+
+    // Event listener for scrolling
+    window.addEventListener('scroll', activateSection);
+
+    document.getElementById("hamburger").addEventListener("click", function () {
+        let menu = document.querySelector("header");
+
+        if (menu.classList.contains("open")) {
+            menu.classList.remove("open");
+        } else {
+            menu.classList.add("open");
         }
     });
 
-    navLinks.forEach(link => {
-        link.classList.remove('active');
-        if (link.getAttribute('href').includes(currentSection)) {
-            link.classList.add('active');
-        }
+    document.getElementById("main-wrap").addEventListener("click", function () {
+        document.querySelector("header").classList.remove("open");
     });
-}
-
-// Event listener for scrolling
-window.addEventListener('scroll', activateSection);
+});
